38954cc6afc73f533c9cd81f4513b640b02bc9ec
voltaire33010 <voltaire33010@gmail.com>
2018-12-15 14:44:07 -0500

    git api

---
+++ Data/DataContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+            
+        }
+        
+        public DbSet<Value> Values { get; set; }
+    }
+}
