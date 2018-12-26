45c3c61a51043c660300d7dc2a0b55aa0666e10f
voltaire33010 <voltaire33010@gmail.com>
2018-12-25 19:37:17 -0500

    finish authentication

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -19,6 +21,7 @@ namespace DatingApp.API.Controllers
             _context = context;
         }
         // GET api/values
+     
         [HttpGet]
         public async Task<IActionResult>GetValues()
         {
--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,3 +1,4 @@
+using DatingApp.API.Migrations;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Models
@@ -10,5 +11,6 @@ namespace DatingApp.API.Models
         }
         
         public DbSet<Value> Values { get; set; }
+        public  DbSet<User> Users { get; set; }
     }
 }
